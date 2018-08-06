e0191efd913d1f447986c29a049b11d318dfc5a5
graciemzhang <gracie.zhang@lps-students.org>
2018-08-06 19:09:22 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -7,18 +7,22 @@ guest_name = gets.chomp.capitalize
 puts "party name?"
 party_name = gets.chomp.capitalize
 
-puts "date?"
-date = gets.chomp.capitalize
+puts "month?"
+month = gets.chomp.capitalize
 
+puts "day?"
+day = gets.chomp.capitalize
+
+date = month + " " + day
 puts "time?"
 time = gets.chomp.capitalize
 
 puts "host name?"
 host_name = gets.chomp.capitalize
 
-date_before = date.to_i
+day_before = (date.to_i) - 1
 
 
 puts "Dear #{guest_name}, 
 
-You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than "
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{month} #{day_before}"
